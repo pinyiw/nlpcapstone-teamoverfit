1336b5dd69e36a02889ea9cab397fcabdad0181f
Pinyi Wang <pinyiwang123@gmail.com>
2018-04-14 18:07:09 -0700

    twitter api example

--- twitter_api_example.py
+++ twitter_api_example.py
@@ -1,4 +1,5 @@
 from TwitterAPI import TwitterAPI
+import json
 
 consumer_key = 'WzQ61Y2dpCDP5PL2Inq5J4rMr'
 consumer_secret = 'icPhj44wtybuKYKHU5tTtDyP4MRtvBqhAyDDPglzvOX0pUwNNz'
@@ -8,5 +9,11 @@ access_token_secret = 'LyWL8yKorFVk3lwjTrFb9FnoeatRlISUxiYbOG9xVwvq6'
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 r = api.request('search/tweets', {'q':'pizza'})
-for item in r:
-        print(item)
+with open('twitter-output.txt', 'w') as outfile:
+        for i, item in enumerate(r):
+                if i < 1:
+                        print('Keys:')
+                        print(item.keys())
+                        print('\nOne example:')
+                        print(item)
+                outfile.write('{}\n'.format(item))
